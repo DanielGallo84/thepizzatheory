f686dc9ff4f706b0a49ff96bd00131de6d416cee
Andrea Mingot <andreamingot@gmail.com>
2023-10-24 10:24:13 +0200

    pizza terminada

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
-var masa;
-var ingredientes;
+var masa ="";
+var ingredientes=[];
 
 function app(){
     comprarMasa();
+    aniadirIngrediente("mozarella");
+    aniadirIngrediente("tomate");
+    aniadirIngrediente("peperoni");
+    aniadirIngrediente("champignon");
+    hornear();
+    entregar();
+
+}
+function hornear(){
+    console.log("encender el horno");
+    console.log("precalentar 10 min a 220 grados")
+    console.log("hornear la masa");
+    
+        
+}
+function entregar(){
+    console.log("entregar la pizza");
+
+}
+
+
+function aniadirIngrediente(ingrediente){
+    ingredientes.push(ingrediente); 
+    console.log("hemos aniadido el ingrediente "+ ingrediente);
+
 }
 
 function comprarMasa (){
     masa="clásica"; 
-    console.log("Hemos comprado la masa");
-    console.log(masa);
-}
-
-function aniadirIngrediente(){
-
+    console.log("Hemos comprado la masa " + masa);
 }
 
 
 
 
 
+
+
+
+app()
