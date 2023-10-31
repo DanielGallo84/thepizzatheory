8fe791b4593776adc9fb125f65a32886d3d17b8d
Andrea Mingot <andreamingot@gmail.com>
2023-10-31 10:25:01 +0100

    creación pizzas.js

---
+++ pizzas.js
@@ -0,0 +1,15 @@
+export const pizzas= [
+
+    {
+        name:"pepperoni",
+        ingredientes:
+        [ "mozarella","tomate","pepperoni","champinon"
+        ]
+    },
+    {
+        name:"atun",
+        ingredientes:[
+            "mozarella","tomate","atun","champignon"
+        ]
+    }
+]
